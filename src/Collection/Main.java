2ba8d7cb7e1eea9c6d586f0c1c001a0a3bd96dbe
mokhamadisna <mokhammadisna@gmail.com>
2021-01-16 14:05:43 +0700

    y

---
+++ src/Collection/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Collection;
+
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection collection;
+    }    
+}
---
+++ src/Collection/Planet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Collection;
+
+/**
+ *
+ * @author User
+ */
+public class Planet {
+    private String name;
+    private double mass;
+    public Planet(String name, double mass) {
+        this.name = name;
+        this.mass = mass;
+    }
+    @Override
+    public String toString() {
+        return "Planet " + name + ", mass: " + mass;
+    }    
+}
